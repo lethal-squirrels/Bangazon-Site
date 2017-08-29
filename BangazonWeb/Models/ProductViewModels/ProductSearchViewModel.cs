77a0c245f2d2b78097847108a1dd26cd2fba125d
chazhenricks <chazhenricks@gmail.com>
2017-08-29 13:19:49 -0700

    Search Bar working. Need products to fully test

---
+++ BangazonWeb/Models/ProductViewModels/ProductSearchViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Bangazon.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.ProductViewModels
+{
+    public class ProductSearchViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+
+        public string SearchTerms { get; set; }
+
+        public string SearchRadio { get; set; }
+
+    }
+}
