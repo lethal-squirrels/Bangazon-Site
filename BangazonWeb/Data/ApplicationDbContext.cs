7cea3e42a41ee2b1ff6cf1cf3e12f27fe6a69b10
Matt Augsburger <matthewaugsburger@gmail.com>
2017-08-29 15:20:58 -0500

    Add Payment Type is Functional

--- BangazonWeb/Data/ApplicationDbContext.cs
+++ BangazonWeb/Data/ApplicationDbContext.cs
@@ -20,7 +20,8 @@ namespace Bangazon.Data
             base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
+            // Add your customizations after calling base.OnModelCreating(builder)
+
         }
 
         public DbSet<Bangazon.Models.Product> Product { get; set; }
