0a4181c7da342ce0ed57212ebbd8e93295a1d0b9
jaisi <aarti.jaisinghani02@gmail.com>
2017-08-29 07:48:09 -0700

    initial structure

---
+++ BangazonWeb/Models/ApplicationUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Bangazon.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [System.ComponentModel.DataAnnotations.Required]
+        public string FirstName { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public string LastName { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string Phone { get; set; }
+
+        public ICollection<Product> Products;
+    }
+}
