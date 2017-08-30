b32fb1eec5ff82f8ff5460e77faf27c1d1d6755b
Madeline Power <madelineepower@gmail.com>
2017-08-30 07:40:22 -0700

    view model

---
+++ BangazonWeb/Models/AccountViewModels/AccountSettingsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.AccountViewModels
+{
+    public class AccountSettingsViewModel
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Address")]
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
